27e73cdcb923ce01c1d8a9dae07e653776fbc487
LuciaSolari <escluciasolari@gmail.com>
2026-05-21 20:17:28 -0300

    Lucia Solari app.js
    agregue y cambie nombres

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const alumnos = [
   { nombre: "Juan", edad: 20 },
   { nombre: "María", edad: 22 },
+  { nombre: "Lucia", edad: 20},
+  { nombre: "Mateo", edad: 23},
+  { nombre: "Emilia", edad: 21},
+  { nombre: "Blue", edad: 22},
 ]
 
 function mostrarAlumnos() {
